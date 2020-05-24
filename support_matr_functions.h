1316ae7d5aec2219f72b965142ed890276434a16
CODER591 <mazorchyk@gmail.com>
2020-05-24 16:51:20 +0300

    Added matrix substraction

--- support_matr_functions.h
+++ support_matr_functions.h
@@ -40,6 +40,20 @@ std::vector<std::vector<T> > sum_matrices (std::vector<std::vector<T> >&X,std::v
   }
   return matrix;
 }
+template <typename T>
+std::vector<std::vector<T> > substract_matrices (std::vector<std::vector<T> >&X,std::vector<std::vector<T> >&Y) {
+  std::vector<std::vector<T> > matrix(X.size());
+  for (size_t i = 0 ; i < X.size() ; i++ ){
+    matrix[i].resize(X.size());
+  }
+  for (int i = 0; i < X.size(); ++i){
+      for (int j = 0; j < X[i].size(); ++j){
+          matrix[i][j] = X[i][j] - Y[i][j];
+      }
+  }
+  return matrix;
+}
+
 /*
   A - matrix
   b-  vector
@@ -48,12 +62,12 @@ std::vector<std::vector<T> > sum_matrices (std::vector<std::vector<T> >&X,std::v
 template <typename T>
 std::vector<T> matr_vec_multiply(std::vector<std::vector<T> > &Hess, std::vector<T> &Y) {
   std::vector<T> result(Y.size());
-    for(size_t i = 0; i < Hess.size();i++) {
-      for(size_t j = 0; j < Hess[i].size();j++) {
-          result[i] += Hess[i][j]*Y[j];
-      }
+  for(size_t i = 0; i < Hess.size();i++) {
+    for(size_t j = 0; j < Hess[i].size();j++) {
+       result[i] += Hess[i][j]*Y[j];
     }
-    return result;
+  }
+  return result;
 }
 /*
   A - matrix
@@ -65,10 +79,10 @@ std::vector<T> vec_mult_matr(std::vector<T>& Y, std::vector<std::vector<T> >& He
   std::vector<T> result(Y.size());
   for(size_t i = 0; i < Hess.size(); i++) {
     for(size_t j = 0; j < Hess[i].size(); j++) {
-          result[j] += Y[i]* Hess[i][j];
-      }
+       result[j] += Y[i]* Hess[i][j];
+    }
   }
-    return result;
+  return result;
 }
 
 template <typename T>
@@ -77,7 +91,7 @@ void MatrOut(std::vector<std::vector<T> > &matr) {
     for(size_t j = 0; j < matr[i].size();j++) {
        std::cout << matr[i][j] << " ";
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
   }
 }
 
